a4ec696ea84405abff763e855ca091fc6058c489
TheGeneral00 <152641227+TheGeneral00@users.noreply.github.com>
2024-08-18 20:15:06 +0200

    graph.cpp -> graph.h, fixed bugs

--- data/graph.h
+++ data/graph.h
@@ -1,3 +1,6 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
 #include <vector>
 #include <utility>
 #include <unordered_map>
@@ -8,23 +11,24 @@ using namespace std;
 
 class Graph {
 
-public: 
+private: 
     //Adjacency list: maps each genre to a vector of pairs (connected genre and weigth)
     unordered_map<string, vector<pair< string, double>>> adjacencyList;
 
+public:
     //Add an edge to the between  to genres with a givenweigth
     void addEdge(const string genre1, const string genre2, const double weight){
-        adjacencyList[genre1].emplace_back(genre2, weight)
-        adjacencyList[genre2].emplace_back(genre1, weight)
+        adjacencyList[genre1].emplace_back(genre2, weight);
+        adjacencyList[genre2].emplace_back(genre1, weight);
     }
 
-    const vector<pair<string, double>> &  getNeighbors(const string& genre) const {
-        return adjacencyList.at(genre)
+    const vector<pair<string, double>>& getNeighbors(const string& genre) const {
+        return adjacencyList.at(genre);
     }
 
     //Modify weigths of the edges
     void modifyEdges(const string& genre1, const string& genre2, double newWeight) {
-        bool modified = false
+        bool modified = false;
 
         for(auto& neighbor: adjacencyList[genre1]){
             if (neighbor.first == genre2){
@@ -34,9 +38,9 @@ public:
             }
         }
         for (auto& neighbor: adjacencyList[genre2]){
-            if (neighbor.second == genre1){
+            if (neighbor.first == genre1){
                 neighbor.second = newWeight;
-                modified = true
+                modified = true;
                 break;
             }
         }
@@ -46,26 +50,17 @@ public:
         }
     }
 
-    //return the edges of a given genre
-    void getEdges(const string& genre1) const {
-        auto it = adjacencyList.find(genre)
-        if(it != adjacencyList.end()){
-            return it->second;
-        } else {
-        return {}
-        }
-    }
-
     void getWeight(const string& genre1, const string& genre2) const {
         auto it = adjacencyList.find(genre1);
         if (it != adjacencyList.end()){
             const auto& neighbors = it->second;
             for (const auto& neighbor: neighbors){
-                if (neighbor == genre2){
+                if (neighbor.first == genre2){
                     return neighbor.second;
                 }
             }
         }
+        return -1;
 
     }
     void printGraph() const {
@@ -74,8 +69,10 @@ public:
             const auto& neighbors = node.second;
             cout << "Genre: " << genre << "\n";
             for (const auto& neighbor : neighbors){
-                cout << "Connected to: " << neighbor.first << "with weight" << neigbor.second;
+                cout << "Connected to: " << neighbor.first << " with weight " << neighbor.second;
             } 
         }
     }
-}
+};
+
+#endif // GRAPH_H
