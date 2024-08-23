a62675323c2ee6d18d6f0c9f8d1053cb7ecdb02c
TheGeneral00 <152641227+TheGeneral00@users.noreply.github.com>
2024-08-23 13:59:31 +0200

    added first GUI elements to the setup

--- GUI/setup.py
+++ GUI/setup.py
@@ -1,9 +1,28 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.graphics import Color, Rectangle
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
 
-class Root(BoxLayout):
+
+class AutoSzieButton(Button):
+    def on_size(self, args):
+        super().on_size(*args)
+        self._update_text_size()
+
+    def _update_text_size(self):
+        if self.text:
+            #Reduce text size until text fits the button
+            font_size = self.height/2
+            while font_size > 5:
+                self.font_size = font_size
+                text_width = self.texture_size[0]
+                text_height = self.texture_size[1]
+                if text_width <= self.width and text_height <= self.height:
+                    break
+                font_size -= 1
+
+class Root(FloatLayout):
     pass
 
 class RecommendationToolApp(App):
