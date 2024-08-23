6867191253edd5efba720d282a2d6dfbeb8b9a02
TheGeneral00 <152641227+TheGeneral00@users.noreply.github.com>
2024-08-23 13:15:28 +0200

    started kivy setup with kivy language

--- GUI/setup.py
+++ GUI/setup.py
@@ -1,9 +1,14 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.graphics import Color, Rectangle
+from kivy.uix.boxlayout import BoxLayout
 
-class myApp(App):
+class Root(BoxLayout):
+    pass
+
+class RecommendationToolApp(App):
     def build(self):
-        return Label(text="Hello World")
+        return Root()
     
 if __name__ == '__main__':
-    myApp().run()
+    RecommendationToolApp().run()
