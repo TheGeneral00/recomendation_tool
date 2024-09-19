137352db9bb7e472a31bba9ea96d531910e2c9eb
TheGeneral00 <richard.beldner@outlook.de>
2024-09-19 10:25:13 +0200

    continued writing tests for new_graph class

--- data/test/graph_test.cpp
+++ data/test/graph_test.cpp
@@ -3,6 +3,8 @@
 #include "new_graph.h"
 #include <vector>
 #include <string>
+#include "movie.h"
+#include "show.h"
 
 TEST_CASE("Graph is being tested", "[graph]") {
     
@@ -20,6 +22,25 @@ TEST_CASE("Graph is being tested", "[graph]") {
         for (const std::string& genre : genres) {
             REQUIRE(graph.hasNode(genre));  // Assuming hasNode is a method that checks for node existence
         }
+    }
+    SECTION("adding and modifying edges"){
+        graph.addEdge("action", "comdey", 0.3);
+        graph.addEdge("action", "romance", 0.5);
+        graph.addEdge("comedy", "romance");
+        REQUIRE(graph.hasEdge("action", "romance", 0.5) == true);
+        REQUIRE(graph.hasEdge("action", "comedy", 0.3) == true);
+        REQUIRE(graph.hasEdge("comedy", "romance", 0) == true);
+        REQUIRE(graph.getWeight("action", "romance") == 0.5)
+        graph.modifyEdge("action", "romance", 0.45);
+        REQUIRE(graph.hasEdge("action", "romance", 0.45));
+    }
+    SECTION("removing Edges"){
+        graph.removeEdge("action", "comedy");
+        REQUIRE(graph.hasEdge("action", "comedy") == false);
+    }
+    SECTION("adding and removing Media to a Node"){
+        Movie movieObj("The Jungle Book", {"children", "adventure"}, 105);
+        Show showObj("The Office", {"sitcom", "comedy", "mockumentary"}, 4468, 188);
     }
 }
 
